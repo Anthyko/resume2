52807f68ed316f2d0f1d87dd471a72f2968d5b34
Anthony <16465475+Anthyko@users.noreply.github.com>
2025-11-30 12:46:49 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import { portfolioIcons } from './utils/portfolio-icons.js';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://anthonyhengy.fr",
+    //site: "https://anthonyhengy.fr",
+    site: "https://anthyko.github.io/resume2/",
     integrations: [
         icon({
             include: portfolioIcons,
