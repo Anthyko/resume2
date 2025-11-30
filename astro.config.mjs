84278ec0e7e61f433493d1327f4be6f1b6782ed2
Anthyko <16465475+Anthyko@users.noreply.github.com>
2025-11-30 09:12:19 +0100

    wip: sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 import icon from 'astro-icon';
 import { portfolioIcons } from './utils/portfolio-icons.js';
 
 // https://astro.build/config
 export default defineConfig({
+    site: "https://anthonyhengy.fr",
     integrations: [
         icon({
             include: portfolioIcons,
-        })]
+        }),
+    sitemap()],
 });
